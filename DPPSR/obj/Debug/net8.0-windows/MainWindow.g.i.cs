f2c2ef6a42ec5999947d690ca63f2aea6d9fa40a
AirOut-Develop <develop@airout.kr>
2025-11-03 18:26:56 +0900

    Update AOIDSClib build artifacts

--- DPPSR/obj/Debug/net8.0-windows/MainWindow.g.i.cs
+++ DPPSR/obj/Debug/net8.0-windows/MainWindow.g.i.cs
@@ -101,7 +101,7 @@ namespace DPPSR {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/DPPSR;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/DPPSR;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
